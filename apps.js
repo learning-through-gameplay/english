931b15b27e9041b36309cceb792dbe01c1525ebf
nadav <nadavdorony@gmail.com>
2024-07-30 07:20:58 +0300

    Move logout to user page

--- apps.js
+++ apps.js
@@ -9,6 +9,6 @@ apps =  {
         {icon: 'format_shapes', name:'מצא זוג אותיות', type: 'app', appType: 'common', listName: 'ABC', a: 'englishUpperCase', b:'englishLowerCase', setItems: 10},
         {icon: 'format_shapes', name:'אותיות מבלבלות', type: 'app', appType: 'mcq', listName: 'CONFUSING_LETTERS', questionIndex: 'audio', resultIndex:'confusingLetter', setItems: 5},
         {icon: 'format_shapes', name:'כתיבת אותיות', type: 'app', appType: 'draw_letter', listName: 'ABC', questionIndex: 'audio', resultIndex:'englishUpperCase', setItems: 5, title:'כתוב את האות הגדולה'},
-        {icon: 'format_shapes', name:'אותיות נופלות', type: 'app', appType: 'falling_answers', listName: 'ABC', questionIndex: 'audio', resultIndex:'englishUpperCase', setItems: 5, title:'כתוב את האות הגדולה'},
+        {icon: 'format_shapes', name:'אותיות נופלות', type: 'app', appType: 'falling_answers', listName: 'ABC', questionIndex: 'audio', resultIndex:'englishUpperCase', setItems: 5, title:'אותיות נופלות'},
       ]
     };
